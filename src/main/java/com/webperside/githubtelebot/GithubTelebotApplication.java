51c5dbcba446faf24304a931f171f74f0c7c5b35
hamidsultanzadeh <sultanzade.06@mail.ru>
2023-10-21 11:13:23 +0400

    Initial

---
+++ src/main/java/com/webperside/githubtelebot/GithubTelebotApplication.java
@@ -0,0 +1,13 @@
+package com.webperside.githubtelebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubTelebotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubTelebotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webperside/githubtelebot/GithubTelebotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webperside.githubtelebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubTelebotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
